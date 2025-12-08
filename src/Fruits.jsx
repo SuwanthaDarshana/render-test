c98524b9ce5ee570824ae31d803852151f0f41e1
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-08 22:48:02 +0530

    add ternary operator

--- src/Fruits.jsx
+++ src/Fruits.jsx
@@ -4,7 +4,16 @@ import React from 'react'
 const fruits = ['Mango', 'Orange', 'Banana', 'Grapes'];
 const cars = ['BMW', 'Audi', 'Mercedes', 'Volvo'];
 
+
 function Fruits() {
+
+  const message = ()=>{
+    alert("Hello from Fruits Component");
+  }
+
+  const x = 10;
+
+
   return (
     <div>
         <ul>
@@ -13,6 +22,8 @@ function Fruits() {
         <ol>
             {cars.map(cars=><li key={cars}>{cars}</li>)}
         </ol>
+        <button onClick={message} disabled={true}>Click Me</button>
+        <h1>{(x) < 20 ? "Banana" : "Apple" }</h1>
     </div>
   )
 }
