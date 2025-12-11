ad9c9d1b200d5eca518ffe3809c6c4d03ccdb2b8
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-11 23:11:34 +0530

    add event

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import "./App.css";
 import Car from "./components/Car";
+import SearchForm from "./components/SearchForm";
 import Customer from "./Customer";
 import Fruits from "./Fruits";
 
 function App() {
-
-  const customers =[
-    {id: 1,name: "Jhon Cena", salary: "50000"},
-    {id: 2,name: "Jack Wilson", salary: "60000"},
-    {id: 3,name: "Alice Johnson", salary: "70000"},
-
-  ]
-
+  const customers = [
+    { id: 1, name: "Jhon Cena", salary: "50000" },
+    { id: 2, name: "Jack Wilson", salary: "60000" },
+    { id: 3, name: "Alice Johnson", salary: "70000" },
+  ];
 
   return (
-    <>
-    {customers.map((customer)=>{
-      const {name,salary,id} = customer;
-      return <Customer key={id} name={name} salary={salary}/>
-    })}
-      
-    </>
+    <div>
+      <SearchForm />
+      {customers.map((customer) => {
+        return <Customer {...customer} key={customer.id} />;
+      })}
+    </div>
   );
 }
 
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SearchForm() {
+  return (
+    <div className='search-outer'>
+        <form action="">
+            <input type="search" /> &nbsp;
+            <button type="submit">Search</button>
+        </form>
+    </div>
+  )
+}
+
+export default SearchForm
