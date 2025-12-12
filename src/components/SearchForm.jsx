ed19162114b9edf4d724e60e398113637b7cc097
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-12-12 22:15:29 +0530

    update

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function SearchForm() {
+  const manageChangeEvent = () => {
+    console.log("Change Event Managed");
+  }
+
+  const manageClickEvent = () => {
+    console.log("Click Event Managed");
+  }
+
   return (
-    <div className='search-outer'>
-        <form action="">
-            <input type="search" /> &nbsp;
-            <button type="submit">Search</button>
-        </form>
+    <div className="search-outer">
+      <form action="">
+        <input type="search" onChange={manageChangeEvent} /> &nbsp;
+        <button type="button" onClick={manageClickEvent}>Search</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SearchForm
+export default SearchForm;
