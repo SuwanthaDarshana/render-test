965abfab42027eeceeea46f1bb03820931d4081a
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2026-01-03 23:56:25 +0530

    add controlled form

--- src/components/Forms/MyForm.jsx
+++ src/components/Forms/MyForm.jsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function MyForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with Email:", email, "and Password:", password);
+  };
 
   return (
     <div>
-        <form>
-            <label>Enter Your Name:</label>
-            <input type="text" id="name" name="name" />
-            <input type='text' ></input>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <label>Enter Your Name:</label>
+        <input type="text" id="name" name="name" />
+        <br />
+        <br />
+        <label>Enter Your Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <label>Enter Your Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /> <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default MyForm
+export default MyForm;
