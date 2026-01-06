a7ad1fe9d15e2058de13c7d5908691e26dd49957
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2026-01-07 00:44:44 +0530

    update radio button

--- src/components/Forms/RadioButton.jsx
+++ src/components/Forms/RadioButton.jsx
@@ -1,23 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function RadioButton() {
+  const [gender, setGender] = useState("male");
 
-    const[gender, setGender] = useState('');
-
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    alert(`your gender is :${gender}`);
+  }
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
         <h3>RadioButton</h3>
-        <input
-         type="radio"
-         name='gender'
-         value="male"
-         checked={gender==="male"}
-            onChange={e => setGender(e.target.value)}
-
-         />
+      <p>Select Your Gender</p>
+      <label>Male</label>
+      <input
+        type="radio"
+        name="gender"
+        value="male"
+        checked={gender === "male"}
+        onChange={(e) => setGender(e.target.value)}
+      />
+      <label>Female</label>
+      <input
+        type="radio"
+        name="gender"
+        value="female"
+        checked={gender === "female"}
+        onChange={(e) => setGender(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default RadioButton
+export default RadioButton;
